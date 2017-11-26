5292001df82e3b0cace055cf792407939065ff25
Samal Roman <roman.samal.bs@gmail.com>
2017-11-27 01:46:49 +0300

    Add connect and disconnect functions.

--- IapdWireless/WirelessNetwork.cpp
+++ IapdWireless/WirelessNetwork.cpp
@@ -86,3 +86,25 @@ string WirelessNetwork::calculateQuality(ULONG quality)
 	result += "% (" + to_string(db) + " dbm)";
 	return result;
 }
+
+bool WirelessNetwork::connect(HANDLE handle, GUID guid, WLAN_AVAILABLE_NETWORK avNet, string name, string password)
+{
+	WLAN_CONNECTION_PARAMETERS params;
+	string profile("<?xml version=\"1.0\"?><WLANProfile xmlns=\"http://www.microsoft.com/networking/WLAN/profile/v1\"><name>" + name + "</name><SSIDConfig><SSID><name>" + name + "</name></SSID></SSIDConfig><connectionType>ESS</connectionType><connectionMode>auto</connectionMode><autoSwitch>false</autoSwitch><MSM><security><authEncryption><authentication>WPA2PSK</authentication><encryption>AES</encryption><useOneX>false</useOneX></authEncryption><sharedKey><keyType>passPhrase</keyType><protected>false</protected><keyMaterial>" + password + "</keyMaterial></sharedKey></security></MSM></WLANProfile>");
+	wstring wide(profile.begin(), profile.end());
+	params.wlanConnectionMode = wlan_connection_mode_temporary_profile;
+	params.pDot11Ssid = &avNet.dot11Ssid;
+	params.pDesiredBssidList = NULL;
+	params.strProfile = wide.c_str();
+	params.dot11BssType = avNet.dot11BssType;
+	params.dwFlags = WLAN_CONNECTION_HIDDEN_NETWORK;
+
+	int res = WlanConnect(handle, &guid, &params, NULL);
+	return res == 0;
+}
+
+void WirelessNetwork::disconnect(HANDLE handle, GUID guid)
+{
+	WlanDisconnect(handle, &guid, NULL);
+	return;
+}
--- IapdWireless/WirelessNetwork.h
+++ IapdWireless/WirelessNetwork.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <Windows.h>
 #include <string>
+#include <wchar.h>
 #include <Wlanapi.h>
 #pragma comment(lib, "Wlanapi.lib")
 
@@ -16,6 +17,8 @@ private:
 public:
 	static string calculateQuality(ULONG quality);
 	static string getAuthType(DWORD dot11_code);
+	static bool connect(HANDLE handle, GUID guid, WLAN_AVAILABLE_NETWORK avNet, string name, string password);
+	static void disconnect(HANDLE handle, GUID guid);
 	string getName();
 	string getMAC();
 	string getQuality();
